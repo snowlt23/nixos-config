7728f0f42954a968d89cac976f33f9eb4d9ccd9a
snowlt23 <shsnow23@gmail.com>
2020-03-25 11:37:25 +0900

    Update

--- desktop-configuration.nix
+++ desktop-configuration.nix
@@ -4,8 +4,31 @@
 {
   boot.kernelPackages = pkgs.linuxPackages_latest;
   # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  #boot.loader.systemd-boot.enable = true;
+  #boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.loader = {
+    efi = {
+      canTouchEfiVariables = true;
+      efiSysMountPoint = "/boot";
+    };
+    grub = {
+      devices = [ "nodev" ];
+      efiSupport = true;
+      enable = true;
+      extraEntries = ''
+        menuentry "Windows" {
+          insmod part_gpt
+          insmod fat
+          insmod search_fs_uuid
+          insmod chain
+          search --fs-uuid --set=root 08C8-EFF8
+          chainloader /efi/Microsoft/Boot/bootmgfw.efi
+        }
+      '';
+      version = 2;
+    };
+  };
 
   # Bluetooth
   hardware.bluetooth.enable = true;
